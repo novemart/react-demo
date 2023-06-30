76827f2e9bc86e11b2c240a8e22b6d2e17da8f74
Martina Yusuf <martina.yusuf2@gmail.com>
2023-06-30 14:28:54 +0100

    react demo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import AllHeroes from './components/AllHeroes';
+import { Routes, Route } from "react-router-dom";
+import HeroForm from './components/HeroForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Heroes</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/allheroes">All Heroes</Nav.Link>
+              <Nav.Link href="/addhero">Add Hero</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <Routes>
+          <Route path="allheroes" element={<AllHeroes/>} />
+          <Route path="addhero" element={<HeroForm/>} />
+          <Route path="/" element={<AllHeroes/>} />
+        </Routes>
+      
+    </>
   );
 }
 
